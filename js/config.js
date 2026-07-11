c40b6b4fc67865bd79738fae94bc7f1403c9efb1
orlandojb21-source <orlandojb.21@gmail.com>
2026-07-11 08:20:26 -0500

    Update development Apps Script URL in config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 
 const ENVIRONMENT = "DEV"; // "DEV" | "PROD"
 
-const APPS_SCRIPT_URL_DEV  = "PEGAR_AQUI_URL_APPS_SCRIPT_DEV";
+const APPS_SCRIPT_URL_DEV  = "https://script.google.com/macros/s/AKfycbxvPgZ_qKHvmY0SQQlk2SCMFFgOH4Ae9c7d59Tt8oXB4bDKCcbhB84AgRxMeGzJ2spsPw/exec";
 const APPS_SCRIPT_URL_PROD = "PEGAR_AQUI_URL_APPS_SCRIPT_PROD";
 
 const APPS_SCRIPT_URL = ENVIRONMENT === "PROD"
